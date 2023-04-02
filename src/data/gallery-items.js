dc43a114d1422feb3ad6e23d19a4d3bb9238bf44
Aleksandr-Sidorskiy <you@example.comsanu43967@gmail.com>
2023-04-02 21:30:27 +0300

    slider

---
+++ src/data/gallery-items.js
@@ -0,0 +1,32 @@
+const galleryItems = [
+  {
+    img: '../images/product/flat-lay-of-bath-concept-with-leaves.png',
+    button: 'Акції',
+  },
+  {
+    img: '../images/product/top-view-of-a-cream-and-leaf-with-white-background.png',
+    button: 'Для обличчя',
+  },
+  {
+    img: '../images/product/—Pngtree—green leaf and white cosmetic_1451127 1.png',
+    button: 'Для тіла',
+  },
+  {
+    img: '../images/product/—Pngtree—lovely pearl cosmetic bottle beauty_1451106 1.png',
+    button: 'Для волосся',
+  },
+  {
+    img: '../images/product/—Pngtree—white cosmetic bottles on green_1451101 1.png',
+    button: 'Акції',
+  },
+  {
+    img: '../images/product/top-view-of-a-cream-and-leaf-with-white-background.png',
+    button: 'Акції',
+  },
+  {
+    img: '../images/product/—Pngtree—green leaf and white cosmetic_1451127 1.png',
+    button: 'Акції',
+  },
+];
+
+export default galleryItems;
